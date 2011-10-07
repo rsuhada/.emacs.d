6451b0e36bca2a283dd8971c24de34dda958d460
rsuhada <suhada.robert@gmail.com>
2011-10-07 22:39:22 +0200

    fixed backups gonna try autopair delim is disabled

--- rs_settings/global.el
+++ rs_settings/global.el
@@ -43,22 +43,6 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ivisible backup files
-
-(setq backup-directory-alist '(("." . "~/.emacs-backups")))
-(defvar backup-directory "~/.emacs-backups/")
-
-(setq auto-save-interval 100)
-(setq auto-save-timeout 15)
-(setq make-backup-files         t ;backup my files
-      backup-by-copying         t ;don't clobber symlinks
-      delete-old-versions       t
-      kept-new-versions         10
-      kept-old-versions         5
-      version-control           t ;use versioned backups
-      vc-make-backup-files      t ;make backups for cvs projects
-      vc-follow-symlinks        t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; zsh mode
@@ -301,3 +285,4 @@
 
 (require 're-builder)
 (setq reb-re-syntax 'string)
+
