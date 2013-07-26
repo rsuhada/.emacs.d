411f13d510d8128c2f29d99481fdffbc3985f0b0
rsuhada <suhada.robert@gmail.com>
2013-07-26 10:46:20 +0200

    occur now has all the cool features - cnvenient code browsing, quiting to restore state

--- rs_settings/global.el
+++ rs_settings/global.el
@@ -883,13 +883,13 @@ Does nothing if `visual-line-mode' is on."
     ;; you may want to maximize the buffer
     (delete-other-windows)))
 
+
+(defadvice occur-1 (before occur-maximize activate)
+  (window-configuration-to-register ?>)) ; use some uncommon reg name
+
 (defadvice occur-1 (after occur-maximize activate)
   (other-window 1)
   (next-line 1))
-
-;; (defadvice occur-1 (before occur-maximize activate)
-;;   (window-configuration-to-register ?y))
-
 
 (defun occur-mode-quit ()
   "Quit and close occur window. I want to press 'q' and leave things as they were before in regard of the split of windows in the frame.
@@ -900,12 +900,21 @@ of any of the matches found in occur."
   (switch-to-buffer "*Occur*")
   ;; in order to know where we put the cursor thay might have jumped from qoccur
   (other-window 1)                  ;; go to the main window
-  (point-to-register ?1)            ;; store the latest cursor position
+  (point-to-register ?<)            ;; store the latest cursor position
   (switch-to-buffer "*Occur*")      ;; go back to the occur window
-  (kill-buffer "*Occur*")           ;; delete it
-  (jump-to-register ?y)             ;; reset the original frame state
-  (register-to-point ?1))           ;; re-position cursor
+  ;; (kill-buffer "*Occur*")           ;; FIXME: test if should or not
+  (jump-to-register ?>)             ;; reset the original frame state
+  (register-to-point ?<))           ;; re-position cursor
 
+(defun occur-browse-next ()
+  (interactive)
+  (next-line 1)
+  (occur-mode-display-occurrence))
+
+(defun occur-browse-previous ()
+  (interactive)
+  (previous-line 1)
+  (occur-mode-display-occurrence))
 
 ;; some key bindings defined below. Use "p" ans "n" as in dired mode (without Cntrl key) for previous and next line; just show occurrence without leaving the "occur" buffer; use RET to display the line of the given occurrence, instead of jumping to i,t which you do clicking instead;  also quit mode with Ctrl-g.
 (define-key occur-mode-map (kbd "q") 'occur-mode-quit)
@@ -916,3 +925,5 @@ of any of the matches found in occur."
 (define-key occur-mode-map (kbd "RET") 'occur-mode-display-occurrence)
 (define-key occur-mode-map (kbd "p") 'previous-line)
 (define-key occur-mode-map (kbd "n") 'next-line)
+(define-key occur-mode-map (kbd "M-n") 'occur-browse-next)
+(define-key occur-mode-map (kbd "M-p") 'occur-browse-previous)
